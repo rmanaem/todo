16cb33895a540b9ff16ae5ab5557ec9c00105d55
rmanaem <rmanaem@protonmail.ch>
2021-01-18 13:25:51 -0500

    set up user registration via POST method

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 def signupuser(request):
-    return render(request, 'signupuser.html', {'form': UserCreationForm()})
+    if request.method == 'GET':
+        return render(request, 'signupuser.html', {'form': UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(
+                request.POST['username'], password=request.POST['password1'])
+            user.save()
+        else:
+            # Tell the user the passwords didn't match
+            pass
