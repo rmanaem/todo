467746ef4cc44b2b863b82b8dde4a7eb62d52fd7
rmanaem <rmanaem@protonmail.ch>
2021-01-18 13:01:21 -0500

    Added sign up form from Django forms

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 
 def signupuser(request):
-    return render(request, 'signupuser.html')
+    return render(request, 'signupuser.html', {'form': UserCreationForm()})
